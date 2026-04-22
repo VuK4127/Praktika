4e4be4247586db01decd00220e8ee52e4006351d
VuK4127 <vuk4715@gmail.com>
2026-04-22 22:23:28 +0300

    4 етап

--- Praktiaka2/Praktiaka2/Views/GamePage.xaml.cs
+++ Praktiaka2/Praktiaka2/Views/GamePage.xaml.cs
@@ -1,26 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Praktiaka2.Views
 {
-    /// <summary>
-    /// Interaction logic for GamePage.xaml
-    /// </summary>
     public partial class GamePage : Page
     {
         public GamePage()
         {
             InitializeComponent();
+        }
+
+        private void CheckBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Отримуємо відповідь, прибираємо зайві пробіли
+            string userAnswer = AnswerInput.Text.Trim().ToLower();
+
+            // Тимчасова перевірка для 4 етапу
+            if (userAnswer == "прекрасний")
+            {
+                MessageBox.Show("Правильно! Відповідь вірна.", "Успіх");
+                NextBtn.IsEnabled = true;  // Активуємо кнопку "Наступне"
+                CheckBtn.IsEnabled = false; // Деактивуємо кнопку перевірки
+            }
+            else
+            {
+                MessageBox.Show("Неправильно. Спробуйте ще раз!", "Помилка");
+            }
+        }
+
+        private void NextBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Логіка переходу до наступного питання (для Етапу 5)
+            AnswerInput.Clear();
+            CheckBtn.IsEnabled = true;
+            NextBtn.IsEnabled = false;
+
+            MessageBox.Show("Наступне питання завантажується... ", "Інфо");
         }
     }
 }
